1697f6e56a626f5e9198ef91d396e62a36140a0c
pifchick <70753421+pifchick@users.noreply.github.com>
2021-05-19 04:15:27 +0300

    Add files via upload

---
+++ proectTaxi/proectTaxi/src/main/java/com/taxifinal/proectTaxi/ProectTaxiApplication.java
@@ -0,0 +1,14 @@
+package com.taxifinal.proectTaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ProectTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProectTaxiApplication.class, args);
+	}
+
+}
---
+++ proectTaxi/proectTaxi/src/test/java/com/taxifinal/proectTaxi/ProectTaxiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.taxifinal.proectTaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+@ComponentScan(basePackages = {"java/com/taxifinal/proectTaxi/controller/BlogController.java"})
+class ProectTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
