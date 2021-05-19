44748af514bc3ed60b3d311ff2ea7f928a43a544
pifchick <70753421+pifchick@users.noreply.github.com>
2021-05-19 04:16:44 +0300

    Delete proectTaxi/proectTaxi directory

--- proectTaxi/proectTaxi/src/main/java/com/taxifinal/proectTaxi/repo/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taxifinal.proectTaxi.repo;
-
-import com.taxifinal.proectTaxi.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
